0efa485075ef3b523ca7c171561869db7103f5d7
Kushagra-Rode27 <79641134+Kushagra-Rode27@users.noreply.github.com>
2021-02-28 00:35:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,30 @@ from newsapi import NewsApiClient
 app = Flask(__name__)
 @app.route('/')
 def Index():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     topheadlines = newsapi.get_top_headlines(sources="the-times-of-india")
  
  
     articles = topheadlines['articles']
  
-    desc = []
+    desc =[]
     news = []
-    img = []
- 
- 
+    img =[]
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
- 
- 
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
- 
- 
- 
-    mylist = zip(news, desc, img)
- 
+        url.append(myarticles['url'])
+
+    mylist = zip(news,desc,img,url)
  
     return render_template('index.html', context = mylist)
 
 @app.route('/Sports')
 def sports():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     top_headlines = newsapi.get_top_headlines(country= 'in', category='sports')
     
     articles = top_headlines['articles']
@@ -39,19 +34,20 @@ def sports():
     desc =[]
     news = []
     img =[]
-    
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,url)
     return render_template('Sports.html', context = mylist)
 
 @app.route('/Health')
 def health():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     top_headlines = newsapi.get_top_headlines(category='health')
     
     articles = top_headlines['articles']
@@ -59,19 +55,20 @@ def health():
     desc =[]
     news = []
     img =[]
-    
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,url)
     return render_template('Health.html', context = mylist)
 
 @app.route('/Business')
 def business():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     top_headlines = newsapi.get_top_headlines(category='business')
     
     articles = top_headlines['articles']
@@ -79,19 +76,20 @@ def business():
     desc =[]
     news = []
     img =[]
-    
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,url)
     return render_template('Business.html', context = mylist)
 
 @app.route('/Science')
 def science():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     top_headlines = newsapi.get_top_headlines(category='science')
     
     articles = top_headlines['articles']
@@ -99,19 +97,20 @@ def science():
     desc =[]
     news = []
     img =[]
-    
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,url)
     return render_template('Science.html', context = mylist)
 
 @app.route('/Technology')
 def technology():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     top_headlines = newsapi.get_top_headlines(category='technology')
     
     articles = top_headlines['articles']
@@ -119,67 +118,40 @@ def technology():
     desc =[]
     news = []
     img =[]
-    
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        url.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,url)
     return render_template('Technology.html', context = mylist)
 
 
 @app.route('/index')
 def index():
-    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    newsapi = NewsApiClient(api_key="3c1dc6073964421984cc0533496eafa0")
     topheadlines = newsapi.get_top_headlines(sources="the-times-of-india")
  
  
     articles = topheadlines['articles']
  
-    desc = []
+    desc =[]
     news = []
-    img = []
- 
- 
+    img =[]
+    url = []
     for i in range(len(articles)):
         myarticles = articles[i]
- 
- 
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
- 
- 
- 
-    mylist = zip(news, desc, img)
- 
+        url.append(myarticles['url'])
+
+    mylist = zip(news, desc, img,url)
  
     return render_template('index.html', context = mylist)
-# @app.route('/bbc')
-# def bbc():
-#     newsapi = NewsApiClient(api_key="YOUR-API-KEY")
-#     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
- 
-#     articles = topheadlines['articles']
- 
-#     desc = []
-#     news = []
-#     img = []
- 
-#     for i in range(len(articles)):
-#         myarticles = articles[i]
- 
-#         news.append(myarticles['title'])
-#         desc.append(myarticles['description'])
-#         img.append(myarticles['urlToImage'])
- 
-#     mylist = zip(news, desc, img)
- 
-#     return render_template('bbc.html', context=mylist)
- 
- 
  
 if __name__ == "__main__":
     app.run(debug=True)
