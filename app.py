3620051e093b37e17fa722c41179eae983cd2c7d
atharvadabli <78368831+atharvadabli@users.noreply.github.com>
2021-02-27 11:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,57 @@
+from flask import Flask, render_template
+from newsapi import NewsApiClient
+app = Flask(__name__)
+@app.route('/')
+def Index():
+    newsapi = NewsApiClient(api_key="f44af8f5994845f290267a5cefd35bf3")
+    topheadlines = newsapi.get_top_headlines(sources="the-times-of-india")
+ 
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+ 
+ 
+    mylist = zip(news, desc, img)
+ 
+ 
+    return render_template('index.html', context = mylist)
 
+# @app.route('/bbc')
+# def bbc():
+#     newsapi = NewsApiClient(api_key="YOUR-API-KEY")
+#     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+ 
+#     articles = topheadlines['articles']
+ 
+#     desc = []
+#     news = []
+#     img = []
+ 
+#     for i in range(len(articles)):
+#         myarticles = articles[i]
+ 
+#         news.append(myarticles['title'])
+#         desc.append(myarticles['description'])
+#         img.append(myarticles['urlToImage'])
+ 
+#     mylist = zip(news, desc, img)
+ 
+#     return render_template('bbc.html', context=mylist)
+ 
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
